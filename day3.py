69da19fbfe20cfc93ed8359e2e7bd5f9a4b9ab3a
Antonio <garofalo.antonio1995@gmail.com>
2022-12-03 15:20:17 +0100

    day3 part2 completed

--- day3.py
+++ day3.py
@@ -8,4 +8,24 @@ with open("inputs/day3.txt") as file:
         elif(a[0].islower()):
             score+= ord(a[0])-96
     print(score)
+
+#part2
+with open("inputs/day3.txt") as file:
+    score = 0
+    rucksacks = []
+    rucksacksGroup = []
+    i = 0
+    for line in file:
+        rucksacks.append(line.rstrip())
+        i=i+1
+        if(i == 3):
+            i=0
+            a=list(set(rucksacks[0])&set(rucksacks[1])&set(rucksacks[2]))
+            rucksacks = []
+            if(a[0].isupper()):
+                score+= ord(a[0])-38
+            elif(a[0].islower()):
+                score+= ord(a[0])-96
+    print(score)
+
         
